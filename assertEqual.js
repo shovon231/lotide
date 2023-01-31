31b0c9ab656d9157294b52685f8f2e5266b13ffc
Mohammad Razaul Islam <shovon231@gmail.com>
2023-01-31 10:59:30 +0000

    adding the litterals

--- assertEqual.js
+++ assertEqual.js
@@ -2,16 +2,12 @@
 const assertEqual = function (actual, expected) {
   console.log(
     actual === expected
-      ? String.fromCodePoint(128512) +
-          "Assertion Passed: " +
-          actual +
-          "===" +
-          expected
-      : String.fromCodePoint(0x1f621) +
-          "Assertion Failed: " +
-          actual +
-          "!==" +
-          expected
+      ? `${String.fromCodePoint(
+          128512
+        )} Assertion Passed: ${actual}===${expected}`
+      : `${String.fromCodePoint(
+          0x1f621
+        )}Assertion Failed : ${actual} !== ${expected}`
   );
 };
 
