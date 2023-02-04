2913fc3c96b4fb0843344b74ff2fb1a1de260fdf
Mohammad Razaul Islam <shovon231@gmail.com>
2023-02-04 08:26:48 -0500

    eqArrays.js reverse.js

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  console.log(
+    actual === expected
+      ? `${String.fromCodePoint(
+          128512
+        )} Assertion Passed: ${actual}===${expected}`
+      : `${String.fromCodePoint(
+          0x1f621
+        )}Assertion Failed : ${actual} !== ${expected}`
+  );
+};
+
+const eqArrays = function (arr1, arr2) {
+  //console.log(arr1.length, arr2.length);
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i <= arr1.length - 1; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
+assertEqual(eqArrays([1, 2], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
---
+++ reverse.js
@@ -0,0 +1,14 @@
+let inputArg = process.argv;
+
+const reverseFunc = function (inputArg) {
+  let inputArr = inputArg.splice(2);
+  console.log(inputArr.length);
+  let result = [];
+  for (let i = 0; i >= inputArr.length; i++) {
+    result.push(inputArr[i]);
+    console.log(result);
+  }
+  return result;
+};
+
+console.log(reverseFunc(inputArg));
