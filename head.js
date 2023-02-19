fbb68ac0dae9af4649320b44f1d9d9641387fd07
Mohammad Razaul Islam <shovon231@gmail.com>
2023-02-19 13:51:49 -0500

    exporting assertEqual and head. create new directory for test cases. create assertEqualTest and headTest for test cases

--- head.js
+++ head.js
@@ -1,23 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  console.log(
-    actual === expected
-      ? `${String.fromCodePoint(
-          128512
-        )} Assertion Passed: ${actual}===${expected}`
-      : `${String.fromCodePoint(
-          0x1f621
-        )}Assertion Failed : ${actual} !== ${expected}`
-  );
-};
+const assertEqual = require("./assertEqual");
 
 const head = function (arr) {
   return arr[0];
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
