9198847bfaafe78e648f1148907d0ebd095e21d8
Mohammad Razaul Islam <shovon231@gmail.com>
2023-02-19 16:41:09 -0500

    Using Mocha and Chai to check the assertion of headTest file

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,26 @@
+const assert = require("chai").assert;
 const head = require("../head");
-const assertEqual = require("../assertEqual");
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 'Lighthouse Labs' for ['Bootcamp']", () => {
+    assert.strictEqual(head(["Lighthouse Labs"]), "Bootcamp");
+  });
+  it("returns '1' for ['1']", () => {
+    assert.strictEqual(head("1"), "1");
+  });
+  it("returns '[5, 6, 7]' for ['5']", () => {
+    assert.strictEqual(head([5, 6, 7]), "5");
+  });
+  it("returns '[Hello, Lighthouse, Labs]' for ['Hello']", () => {
+    assert.strictEqual(head([Hello, Lighthouse, Labs]), "Hello");
+  });
+  it("returns '[]' for ['5']", () => {
+    assert.strictEqual(head([]), "5");
+  });
+});
